adf9a4db31e499f1274af10bd259d2591656a569
Melanie Rodriguez <melarodrigg@gmail.com>
2025-11-21 22:16:50 -0600

    Attempt 1

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -3,23 +3,39 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
+
+  // Initialize state for selected category
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function handleCategoryChange(e) {
+    setSelectedCategory(e.target.value);
   }
 
+  // Initialize state for item to search
+  const [search, setSearch] = useState("");
+
+  function handleSearchChange(e) {
+    setSearch(e.target.value);
+  }
+
+  // Variable to display items based on selected category or search
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+
+  }).filter((item) => {
+    if (! search) {
+      return true
+    };
+    return new RegExp(search, "i").test(item.name);
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}  />
+      <Filter search={search} onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
